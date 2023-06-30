7c62b8d6a0f764418e4d1d91ab40994725bce2c7
Ashy5000 <76829164+Ashy5000@users.noreply.github.com>
2023-06-30 16:54:38 +0000

    Implements DSL if statements

--- dsl/examples/script.dsl
+++ dsl/examples/script.dsl
@@ -1,14 +1,10 @@
-mkvar neuralNetWidth 30
-setvar neuralNetWidth 20
-log getvar neuralNetWidth
-set width getvar neuralNetWidth
-set depth 1
-set rand 0.1
-set rounds 30
-set calibration 0.1
-set round -
-set data [1,2,3,4,5]
-init
-train
+mkvar boolone +
+mkvar booltwo -
+if getvar boolone
+log inner
+if getvar boolone
+log outer
+close
+log back_at_inner
+close
 end
-output
--- dsl/run.js
+++ dsl/run.js
@@ -8,56 +8,71 @@ const path = prompt("Enter file path: ");
 const fileContents = readFileSync(path, { "encoding": "utf8" });
 const lines = fileContents.split("\n");
 
-console.log("File contents:\n" + lines.join("\n"));
-
 let data = [];
 let variables = {};
+let evaluatingIfDepth = 0;
+let codeIfDepth = 0;
 
-for(let i = 0; i < lines.length; i++) {
-  const words = lines[i].split(" ");
-  const operation = words[0];
-  if(operation == "init") {
-    console.log("Initializing...");
-    generateNeuralNet(hiddenLayerSize, neuralNetDepth);
-    console.log("Model ready.");
-  } else if(operation == "set") {
-    let variableName = words[1];
-    let value = words[2] != "getvar" ? words[2] : variables[words[3]];
-    if(variableName == "width") {
-      setHiddenLayerSize(Number(value));
-    } else if(variableName == "depth") {
-      setNeuralNetDepth(Number(value));
-    } else if(variableName == "rand") {
-      setRandRange(Number(value));
-    } else if(variableName == "rounds") {
-      setLearningRounds(Number(value));
-    } else if(variableName == "round") {
-      setRounding(value == "+" ? true : false);
-    } else if(variableName == "data") {
-      data = JSON.parse(value);
-    } else if(variableName == "calibration") {
-      setCalibration(Number(value));
-    } else if(variableName == "finalBias") {
-      setFinalResultBias(Number(value));
+const evaluate = (lines) => {
+  for(let i = 0; i < lines.length; i++) {
+    if(codeIfDepth > evaluatingIfDepth && codeIfDepth > 0) {
+      codeIfDepth--;
+      continue;
     }
-  } else if(operation == "train") {
-    console.log("Training model...");
-    learn(learningRounds, data);
-    console.log("Training succesfully completed.");
-  } else if(operation == "output") {
-    console.log(showResults(data.length + 5).join("\n"));
-  } else if(operation == "mkvar") {
-    variables[words[1]] = words[2] != "getvar" ? words[2] : variables[words[3]];
-  } else if(operation == "setvar") {
-    if(variables[words[1]]) {
-      variables[words[1]] = words[2] != "getvar" ? words[2] : variables[words[2]];
-    } else {
-      console.error("ERR: Variable not found");
+    const words = lines[i].split(" ");
+    const operation = words[0];
+    if(operation == "init") {
+      console.log("Initializing...");
+      generateNeuralNet(hiddenLayerSize, neuralNetDepth);
+      console.log("Model ready.");
+    } else if(operation == "set") {
+      let variableName = words[1];
+      let value = words[2] != "getvar" ? words[2] : variables[words[3]];
+      if(variableName == "width") {
+        setHiddenLayerSize(Number(value));
+      } else if(variableName == "depth") {
+        setNeuralNetDepth(Number(value));
+      } else if(variableName == "rand") {
+        setRandRange(Number(value));
+      } else if(variableName == "rounds") {
+        setLearningRounds(Number(value));
+      } else if(variableName == "round") {
+        setRounding(value == "+" ? true : false);
+      } else if(variableName == "data") {
+        data = JSON.parse(value);
+      } else if(variableName == "calibration") {
+        setCalibration(Number(value));
+      } else if(variableName == "finalBias") {
+        setFinalResultBias(Number(value));
+      }
+    } else if(operation == "train") {
+      console.log("Training model...");
+      learn(learningRounds, data);
+      console.log("Training succesfully completed.");
+    } else if(operation == "output") {
+      console.log(showResults(data.length + 5).join("\n"));
+    } else if(operation == "mkvar") {
+      variables[words[1]] = words[2] != "getvar" ? words[2] : variables[words[3]];
+    } else if(operation == "setvar") {
+      if(variables[words[1]]) {
+        variables[words[1]] = words[2] != "getvar" ? words[2] : variables[words[2]];
+      } else {
+        console.error("ERR: Variable not found");
+      }
+    } else if(operation == "log") {
+      console.log(words[1] != "getvar" ? words[1] : variables[words[2]])
+    } else if(operation == "end") {
+      break;
+    } else if(operation == "if") {
+      codeIfDepth++;
+      if(words[1] == "+" || (words[1] == "getvar" && variables[words[2]] == "+")) {
+        evaluatingIfDepth++;
+      }
+    } else if(operation == "close") {
+      codeIfDepth--;
+      evaluatingIfDepth--;
     }
-  } else if(operation == "log") {
-    console.log(words[1] != "getvar" ? words[1] : variables[words[2]])
-  } else if(operation == "end") {
-    break;
   }
-}
+};
+evaluate(lines);
 console.log("Program finished.");
